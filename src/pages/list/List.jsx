cfae0db2e81d4b8394fab139bd7e33ca782a5784
Crimson-Hat <98809053+Crimson-Hat@users.noreply.github.com>
2022-09-23 10:57:20 -0400

    Update List.jsx

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -13,11 +13,11 @@ const List = () => {
   const [destination] = useState(location.state.destination);
   const [dates, setDates] = useState(location.state.dates);
   const [openDate, setOpenDate] = useState(false);
-  const [options, setOptions] = useState(location.state.options);
+  const [options] = useState(location.state.options);
   const [min, setMin] = useState(undefined);
   const [max, setMax] = useState(undefined);
 
-  const { data, loading, error, reFetch } = useFetch(
+  const { data, loading, reFetch } = useFetch(
     `/hotels?city=${destination}&min=${min || 0 }&max=${max || 999}`
   );
 
