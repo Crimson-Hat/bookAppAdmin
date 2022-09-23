260d802ec56ca82c0104e33cf05b65efefb20122
Crimson-Hat <98809053+Crimson-Hat@users.noreply.github.com>
2022-09-23 10:52:44 -0400

    Update Reserve.jsx

--- src/components/reserve/Reserve.jsx
+++ src/components/reserve/Reserve.jsx
@@ -10,7 +10,7 @@ import { useNavigate } from "react-router-dom";
 
 const Reserve = ({ setOpen, hotelId }) => {
   const [selectedRooms, setSelectedRooms] = useState([]);
-  const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
+  const { data } = useFetch(`/hotels/room/${hotelId}`);
   const { dates } = useContext(SearchContext);
 
   const getDatesInRange = (startDate, endDate) => {
