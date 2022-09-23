cee2b00569ca9bb0b2a1f174085c596e2cad31e9
Crimson-Hat <98809053+Crimson-Hat@users.noreply.github.com>
2022-09-23 10:55:35 -0400

    Update List.jsx

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -10,7 +10,7 @@ import useFetch from "../../hooks/useFetch";
 
 const List = () => {
   const location = useLocation();
-  const [destination, setDestination] = useState(location.state.destination);
+  const [destination] = useState(location.state.destination);
   const [dates, setDates] = useState(location.state.dates);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
