55626ee5e37af6bc965414fda3bacaf5a66a4726
Crimson-Hat <98809053+Crimson-Hat@users.noreply.github.com>
2022-09-23 16:05:02 -0400

    Update NewRoom.jsx

--- src/pages/newRoom/NewRoom.jsx
+++ src/pages/newRoom/NewRoom.jsx
@@ -11,7 +11,7 @@ const NewRoom = () => {
   const [hotelId, setHotelId] = useState(undefined);
   const [rooms, setRooms] = useState([]);
 
-  const { data, loading, error } = useFetch("/hotels");
+  const { data, loading } = useFetch("/hotels");
 
   const handleChange = (e) => {
     setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
