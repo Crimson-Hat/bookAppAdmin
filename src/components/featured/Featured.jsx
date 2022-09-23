23aa6347fbdaf1580b78e41e7d3b89d913d3551c
Crimson-Hat <98809053+Crimson-Hat@users.noreply.github.com>
2022-09-23 10:49:14 -0400

    Update Featured.jsx

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,7 +2,7 @@ import useFetch from "../../hooks/useFetch";
 import "./featured.css";
 
 const Featured = () => {
-  const { data, loading, error } = useFetch(
+  const { data, loading } = useFetch(
     "/hotels/countByCity?cities=berlin,madrid,london"
   );
 
